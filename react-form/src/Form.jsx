4f5805cfdfac188181bef528c632e16c992b91cd
Siya Saxena <saxenasiya9@gmail.com>
2026-07-04 19:21:01 +0530

    Multiple inputs

--- react-form/src/Form.jsx
+++ react-form/src/Form.jsx
@@ -1,15 +1,45 @@
 import { useState } from "react";
 export default function Form() {
-  let [fullName, setFullName] = useState("siya");
+  let [fullName, setFullName] = useState("");
+  let [username, setUsername] = useState("");
   let handleNameChange = (event) => {
-    console.log(event.target);
+    // console.log(event.target.value);
+    setFullName(event.target.value);
+  };
+  let handleUsername = (event) => {
+    // console.log(event.target.value);
+    setUsername(event.target.value);
   };
   return (
     <form>
+      <label htmlFor="fullName">Full Name: </label>
+      {/* for will be not used, since it is used for looping */}
       <input
         placeholder="enter your full name"
         type="text"
         value={fullName}
+        id="fullName"
+        onChange={handleNameChange}
+      />
+
+      <br></br>
+      <br></br>
+
+      <label htmlFor="username">Username: </label>
+      <input
+        placeholder="enter username"
+        type="text"
+        value={username}
+        id="username"
+        onChange={handleUsername}
+      />
+      <button>Submit</button>
+      <label htmlFor="fullName">Full Name: </label>
+      <input
+        placeholder="enter your full name"
+        type="text"
+        value={fullName}
+        id="fullName"
         onChange={handleNameChange}
       />
       <button>Submit</button>
